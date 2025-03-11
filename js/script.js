0c3f5e255b99f9fe1d7fedc6d2fa970e3da6e794
waffleshelter <saturnn54@gmail.com>
2025-03-11 23:45:24 +0500

    финальный результат

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const sideMenu = document.querySelector('.left');
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('.close');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.style.display = "none"
+    sideMenu.style.display = "block";
+})
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = "none";
+    menuBtn.style.display = "inline-block";
+})
